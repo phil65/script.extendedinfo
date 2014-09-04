cfa5be26a5b6cf72e952ad9cd33d1a8616cfd83e
Philipp Te <philipptemminghoff@gmx.de>
2014-09-04 02:30:07 +0200

    miscscraper.py pep8

--- MiscScraper.py
+++ MiscScraper.py
@@ -1,36 +1,43 @@
-import os, re, random, sys, urllib
-import xbmc, xbmcaddon
+import os
+import re
+import random
+import sys
+import urllib
+import xbmc
+import xbmcaddon
 from Utils import *
 if sys.version_info < (2, 7):
     import simplejson
 else:
     import json as simplejson
-    
+
 tvrage_key = 'VBp9BuIr5iOiBeWCFRMG'
 youtube_key = 'AI39si4DkJJhM8cm7GES91cODBmRR-1uKQuVNkJtbZIVJ6tRgSvNeUh4somGAjUwGlvHFj3d0kdvJdLqD0aQKTh6ttX7t_GjpQ'
 bandsintown_apikey = 'xbmc_open_source_media_center'
-Addon_Data_Path = os.path.join( xbmc.translatePath("special://profile/addon_data/%s" % xbmcaddon.Addon().getAddonInfo('id') ).decode("utf-8") )
+Addon_Data_Path = os.path.join(xbmc.translatePath("special://profile/addon_data/%s" % xbmcaddon.Addon().getAddonInfo('id')).decode("utf-8"))
+
 
 def GetXKCDInfo():
     settings = xbmcaddon.Addon(id='script.extendedinfo')
     items = []
-    for i in range(0,10):
+    for i in range(0, 10):
         try:
             url = 'http://xkcd.com/%i/info.0.json' % random.randrange(1, 1190)
             response = GetStringFromUrl(url)
             results = simplejson.loads(response)
             item = {'Image': results["img"],
                     'Title': results["title"],
-                    'Description':results["alt"]  }
+                    'Description': results["alt"]}
             items.append(item)
         except:
             log("Error when setting XKCD info")
     return items
 
+
 def GetCandHInfo():
     count = 1
     images = []
-    for i in range(1,30):
+    for i in range(1, 30):
         try:
             url = 'http://www.explosm.net/comics/%i/' % random.randrange(1, 3128)
             response = GetStringFromUrl(url)
@@ -45,14 +52,15 @@ def GetCandHInfo():
                         dateregex = '[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9][0-9][0-9]'
                         datematches = re.findall(dateregex, response)
                         newitem = {'Image': item,
-                                    'Title': datematches[0]  }
+                                   'Title': datematches[0]}
                         images.append(newitem)
-                        count += 1                      
+                        count += 1
               #  wnd.setProperty('CyanideHappiness.%i.Title' % count, item["title"])
                 if count > 10:
                     break
     return images
-                     
+
+
 def GetFlickrImages():
     images = []
     results = ""
@@ -65,13 +73,13 @@ def GetFlickrImages():
     count = 1
     if results:
         for item in results["value"]["items"]:
-            image = {'Background': item["link"]  }
+            image = {'Background': item["link"]}
             images.append(image)
             count += 1
     return images
-                                       
-            
-def GetYoutubeVideos(jsonurl,prefix = ""):
+
+
+def GetYoutubeVideos(jsonurl, prefix=""):
     results = []
     try:
         response = GetStringFromUrl(jsonurl)
@@ -80,41 +88,42 @@ def GetYoutubeVideos(jsonurl,prefix = ""):
         log("Error when fetching JSON data from net")
     count = 1
     log("found youtube vids: " + jsonurl)
-    videos=[]
+    videos = []
     if results:
         try:
             for item in results["value"]["items"]:
                 video = {'Thumb': item["media:thumbnail"][0]["url"],
                          'Media': ConvertYoutubeURL(item["link"]),
                          'Play': "PlayMedia(" + ConvertYoutubeURL(item["link"]) + ")",
-                         'Title':item["title"],
-                         'Description':item["content"]["content"],
-                         'Date':item["pubDate"]  }
+                         'Title': item["title"],
+                         'Description': item["content"]["content"],
+                         'Date': item["pubDate"]}
                 videos.append(video)
                 count += 1
         except:
             for item in results["feed"]["entry"]:
                 for entry in item["link"]:
-                    if entry.get('href','').startswith('http://www.youtube.com/watch'):
-                        video = {'Thumb': "http://i.ytimg.com/vi/" + ExtractYoutubeID(entry.get('href','')) + "/0.jpg",
-                                 'Media': ConvertYoutubeURL(entry.get('href','')),
-                                 'Play':"PlayMedia(" + ConvertYoutubeURL(entry.get('href','')) + ")",
-                                 'Title':item["title"]["$t"],
-                                 'Description':"To Come",
-                                 'Date':"To Come"  }
+                    if entry.get('href', '').startswith('http://www.youtube.com/watch'):
+                        video = {'Thumb': "http://i.ytimg.com/vi/" + ExtractYoutubeID(entry.get('href', '')) + "/0.jpg",
+                                 'Media': ConvertYoutubeURL(entry.get('href', '')),
+                                 'Play': "PlayMedia(" + ConvertYoutubeURL(entry.get('href', '')) + ")",
+                                 'Title': item["title"]["$t"],
+                                 'Description': "To Come",
+                                 'Date': "To Come"}
                         videos.append(video)
                         count += 1
     return videos
-    
-def GetYoutubeSearchVideos(search_string = "" ,hd = "", orderby = "relevance", time = "all_time"):
+
+
+def GetYoutubeSearchVideos(search_string="", hd="", orderby="relevance", time="all_time"):
     results = []
     if hd and not hd == "false":
         hd_string = "&hd=true"
     else:
         hd_string = ""
-    search_string = urllib.quote(search_string.replace('"',''))
+    search_string = urllib.quote(search_string.replace('"', ''))
     try:
-        url = 'http://gdata.youtube.com/feeds/api/videos?v=2&alt=json&q=%s&time=%s&orderby=%s&key=%s%s' % (search_string, time, orderby, youtube_key,hd_string)
+        url = 'http://gdata.youtube.com/feeds/api/videos?v=2&alt=json&q=%s&time=%s&orderby=%s&key=%s%s' % (search_string, time, orderby, youtube_key, hd_string)
         log(url)
         response = GetStringFromUrl(url)
         results = simplejson.loads(response)
@@ -129,15 +138,15 @@ def GetYoutubeSearchVideos(search_string = "" ,hd = "", orderby = "relevance", t
                          'Description': item["media$group"]["media$description"]["$t"],
                          'Title': item["title"]["$t"],
                          'Author': item["author"][0]["name"]["$t"],
-                         'Date': item["published"]["$t"].replace("T"," ").replace(".000Z","")  }
+                         'Date': item["published"]["$t"].replace("T", " ").replace(".000Z", "")}
                 videos.append(video)
                 count += 1
     return videos
-    
-    
-def GetYoutubeUserVideos(userid = ""):
+
+
+def GetYoutubeUserVideos(userid=""):
     results = []
-    userid = urllib.quote(userid.replace('"',''))
+    userid = urllib.quote(userid.replace('"', ''))
     try:
         url = 'https://gdata.youtube.com/feeds/api/users/%s/uploads?v=2&alt=json' % (userid)
         log(url)
@@ -154,10 +163,11 @@ def GetYoutubeUserVideos(userid = ""):
                      'Description': item["media$group"]["media$description"]["$t"],
                      'Title': item["title"]["$t"],
                      'Author': item["author"][0]["name"]["$t"],
-                     'Date': item["published"]["$t"].replace("T"," ").replace(".000Z","")  }
+                     'Date': item["published"]["$t"].replace("T", " ").replace(".000Z", "")}
             videos.append(video)
             count += 1
     return videos
+
 
 def HandleBandsInTownResult(results):
     events = []
@@ -168,8 +178,8 @@ def HandleBandsInTownResult(results):
             for art in event["artists"]:
                 artists += ' / '
                 artists += art['name']
-                artists = artists.replace(" / ", "",1)        
-            event = {'date': event['datetime'].replace("T", " - ").replace(":00", "",1),
+                artists = artists.replace(" / ", "", 1)
+            event = {'date': event['datetime'].replace("T", " - ").replace(":00", "", 1),
                      'city': venue['city'],
                      'lat': venue['latitude'],
                      'lon': venue['longitude'],
@@ -181,15 +191,16 @@ def HandleBandsInTownResult(results):
              #        'artist_mbid': ,
           #           'status': event['status'],
          #            'ticket_status': event['ticket_status'],
-                     'artists': artists  }
+                     'artists': artists}
             events.append(event)
-        except Exception,e: 
+        except Exception as e:
             log("Exception in HandleBandsInTownResult")
             log(e)
             prettyprint(event)
     return events
-    
-def GetArtistNearEvents(Artists): # not possible with api 2.0
+
+
+def GetArtistNearEvents(Artists):  # not possible with api 2.0
     ArtistStr = ''
     count = 0
   #  prettyprint(Artists)
@@ -201,10 +212,10 @@ def GetArtistNearEvents(Artists): # not possible with api 2.0
             artist = urllib.quote(artist.encode("utf-8"))
         if count < 49:
             if len(ArtistStr) > 0:
-                 ArtistStr = ArtistStr + '&'
+                ArtistStr = ArtistStr + '&'
             ArtistStr = ArtistStr + 'artists[]=' + artist
             count += 1
-    url = 'http://api.bandsintown.com/events/search?%sformat=json&location=use_geoip&api_version=2.0&app_id=%s' % (ArtistStr, bandsintown_apikey)
+    url = 'http://api.bandsintown.com/events/search?%sformat=json&location=use_geoip&radius=50&per_page=100&api_version=2.0&app_id=%s' % (ArtistStr, bandsintown_apikey)
     response = GetStringFromUrl(url)
     results = simplejson.loads(response)
   #   prettyprint(results)
