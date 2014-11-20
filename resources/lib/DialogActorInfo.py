4e4c2c0be7b59f5b71108b3b5299176b40071c43
Philipp Te <philipptemminghoff@gmx.de>
2014-11-20 23:53:21 +0100

    some fixes for actor search

--- resources/lib/DialogActorInfo.py
+++ resources/lib/DialogActorInfo.py
@@ -20,25 +20,30 @@ class DialogActorInfo(xbmcgui.WindowXMLDialog):
             names = name.split(" / ")
             if len(names) > 1:
                 Dialog = xbmcgui.Dialog()
-                ret = Dialog.select("test", names)
-                Notify(str(ret))
-            self.id = GetPersonID(name)
+                ret = Dialog.select("Actor Info", names)
+                if ret == -1:
+                    return False
+                name = names[ret]
+            clean_name = name.split(" as ")[0]
+            self.id = GetPersonID(clean_name)
         xbmc.executebuiltin("ActivateWindow(busydialog)")
         if self.id:
             self.person, self.movie_roles, self.tvshow_roles, self.images = GetExtendedActorInfo(self.id)
-        name = self.person["name"]
-        self.youtube_vids = GetYoutubeSearchVideos(name)
-        homewindow.setProperty("actor.Title", name)
-        homewindow.setProperty("actor.TotalMovies", str(len(self.movie_roles)))
-        homewindow.setProperty("actor.Biography", self.person["biography"])
-        homewindow.setProperty("actor.Birthday", self.person["birthday"])
-        homewindow.setProperty("actor.Thumb", self.person["thumb"])
-        homewindow.setProperty("actor.id", self.person["id"])
-        homewindow.setProperty("actor.AlsoKnownAs", self.person["also_known_as"])
-        homewindow.setProperty("actor.Description", self.person["description"])
-        homewindow.setProperty("actor.PlaceOfBirth", self.person["place_of_birth"])
-        homewindow.setProperty("actor.DeathDay", self.person["deathday"])
-        homewindow.setProperty("actor.Homepage", self.person["homepage"])
+            name = self.person["name"]
+            self.youtube_vids = GetYoutubeSearchVideos(name)
+            homewindow.setProperty("actor.Title", name)
+            homewindow.setProperty("actor.TotalMovies", str(len(self.movie_roles)))
+            homewindow.setProperty("actor.Biography", self.person["biography"])
+            homewindow.setProperty("actor.Birthday", self.person["birthday"])
+            homewindow.setProperty("actor.Thumb", self.person["thumb"])
+            homewindow.setProperty("actor.id", self.person["id"])
+            homewindow.setProperty("actor.AlsoKnownAs", self.person["also_known_as"])
+            homewindow.setProperty("actor.Description", self.person["description"])
+            homewindow.setProperty("actor.PlaceOfBirth", self.person["place_of_birth"])
+            homewindow.setProperty("actor.DeathDay", self.person["deathday"])
+            homewindow.setProperty("actor.Homepage", self.person["homepage"])
+        else:
+            Notify("No ID found")
         xbmc.executebuiltin("Dialog.Close(busydialog)")
 
     def onInit(self):
