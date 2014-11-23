078d3430b518cd4f7dbcf287a7ae4ff1436b6f5e
Philipp Te <philipptemminghoff@gmx.de>
2014-11-23 05:27:19 +0100

    use own xmls

--- default.py
+++ default.py
@@ -8,6 +8,7 @@ import urllib
 
 __addon__ = xbmcaddon.Addon()
 __addonid__ = __addon__.getAddonInfo('id')
+__addonname__ = __addon__.getAddonInfo('name')
 __addonversion__ = __addon__.getAddonInfo('version')
 __language__ = __addon__.getLocalizedString
 __cwd__ = __addon__.getAddonInfo('path').decode("utf-8")
@@ -188,9 +189,8 @@ class Main:
                 if MovieId:
                     passHomeDataToSkin(GetExtendedMovieInfo(MovieId))
             elif info == 'extendedactorinfo':
-                    # dialog = ActorInfoDialog(u'script-%s-ActorInfoDialog.xml' % addon_name, addon_path, data=data)
                     from DialogActorInfo import DialogActorInfo
-                    dialog = DialogActorInfo(u'script-Actors-DialogInfo.xml', __cwd__, id=self.id, name=self.name)
+                    dialog = DialogActorInfo(u'script-%s-DialogInfo.xml' % __addonname__, __cwd__, id=self.id, name=self.name)
                     dialog.doModal()
 
             elif info == 'extendedtvinfo':
