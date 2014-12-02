ba5b3821aa9270647a7872a65d25a77a386d0677
Philipp Te <philipptemminghoff@gmx.de>
2014-12-02 22:10:26 +0100

    forgot to comment out two lines :/

--- resources/lib/DialogVideoInfo.py
+++ resources/lib/DialogVideoInfo.py
@@ -45,8 +45,8 @@ class DialogVideoInfo(xbmcgui.WindowXMLDialog):
             self.youtube_listitems = CreateListItems(self.youtube_vids, 0)
             if self.movie["SetId"]:
                 self.set_listitems = CreateListItems(GetSetMovies(self.movie["SetId"]))
-            json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Files.GetDirectory", "params": {"directory": "videodb://movies/actors/", "media": "files"}, "id": 1}')
-            json_query = unicode(json_query, 'utf-8', errors='ignore')
+            # json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Files.GetDirectory", "params": {"directory": "videodb://movies/actors/", "media": "files"}, "id": 1}')
+            # json_query = unicode(json_query, 'utf-8', errors='ignore')
             # json_response = simplejson.loads(json_query)
             # for db_actor in json_response["result"]["files"]:
             #     for movie_actor in actors:
