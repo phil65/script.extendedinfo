a2e4758166cf98c3a1a8c8e780e90a8069bbecea
Philipp Te <philipptemminghoff@gmx.de>
2014-12-14 04:32:37 +0100

    missed to push new .py

---
+++ resources/lib/ImageTools.py
@@ -0,0 +1,127 @@
+import urllib
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcvfs
+import os
+from Utils import *
+try:
+    from PIL import Image, ImageFilter, ImageOps
+    from ImageOperations import MyGaussianBlur
+except:
+    pass
+
+addon = xbmcaddon.Addon()
+addon_id = addon.getAddonInfo('id')
+addon_icon = addon.getAddonInfo('icon')
+addon_strings = addon.getLocalizedString
+addon_name = addon.getAddonInfo('name')
+addon_path = addon.getAddonInfo('path').decode("utf-8")
+Addon_Data_Path = os.path.join(xbmc.translatePath("special://profile/addon_data/%s" % addon_id).decode("utf-8"))
+homewindow = xbmcgui.Window(10000)
+
+
+def Filter_Image(filterimage, radius):
+    if not xbmcvfs.exists(Addon_Data_Path):
+        xbmcvfs.mkdir(Addon_Data_Path)
+    cachedthumb = xbmc.getCacheThumbName(filterimage)
+    filename = "%s-radius_%i.png" % (cachedthumb, radius)
+    targetfile = os.path.join(Addon_Data_Path, filename)
+    xbmc_vid_cache_file = os.path.join("special://profile/Thumbnails/Video", cachedthumb[0], cachedthumb)
+    xbmc_cache_file = os.path.join("special://profile/Thumbnails", cachedthumb[0], cachedthumb[:-4] + ".jpg")
+    if filterimage == "":
+        return "", ""
+    if not xbmcvfs.exists(targetfile):
+        img = None
+        for i in range(1, 4):
+            try:
+                if xbmcvfs.exists(xbmc_cache_file):
+                    log("image already in xbmc cache: " + xbmc_cache_file)
+                    img = Image.open(xbmc.translatePath(xbmc_cache_file))
+                    break
+                elif xbmcvfs.exists(xbmc_vid_cache_file):
+                    log("image already in xbmc video cache: " + xbmc_vid_cache_file)
+                    img = Image.open(xbmc.translatePath(xbmc_vid_cache_file))
+                    break
+                else:
+                    filterimage = urllib.unquote(filterimage.replace("image://", "")).decode('utf8')
+                    if filterimage.endswith("/"):
+                        filterimage = filterimage[:-1]
+                    log("copy image from source: " + filterimage)
+                    xbmcvfs.copy(filterimage, targetfile)
+                    img = Image.open(targetfile)
+                    break
+            except:
+                log("Could not get image for %s (try %i)" % (filterimage, i))
+                xbmc.sleep(500)
+        if not img:
+            return "", ""
+        img.thumbnail((200, 200), Image.ANTIALIAS)
+        img = img.convert('RGB')
+        imgfilter = MyGaussianBlur(radius=radius)
+        img = img.filter(imgfilter)
+        img.save(targetfile)
+    else:
+        log("blurred img already created: " + targetfile)
+        img = Image.open(targetfile)
+    imagecolor = Get_Colors(img)
+    return targetfile, imagecolor
+
+
+def Get_Colors(img):
+    width, height = img.size
+    pixels = img.load()
+    data = []
+    for x in range(width/2):
+        for y in range(height/2):
+            cpixel = pixels[x*2, y*2]
+            data.append(cpixel)
+    r = 0
+    g = 0
+    b = 0
+    counter = 0
+    for x in range(len(data)):
+        brightness = data[x][0] + data[x][1] + data[x][2]
+        if brightness > 150 and brightness < 720:
+            r += data[x][0]
+            g += data[x][1]
+            b += data[x][2]
+            counter += 1
+    if counter > 0:
+        rAvg = int(r/counter)
+        gAvg = int(g/counter)
+        bAvg = int(b/counter)
+        Avg = (rAvg + gAvg + bAvg) / 3
+        minBrightness = 130
+        if Avg < minBrightness:
+            Diff = minBrightness - Avg
+            if rAvg <= (255 - Diff):
+                rAvg += Diff
+            else:
+                rAvg = 255
+            if gAvg <= (255 - Diff):
+                gAvg += Diff
+            else:
+                gAvg = 255
+            if bAvg <= (255 - Diff):
+                bAvg += Diff
+            else:
+                bAvg = 255
+        imagecolor = "FF%s%s%s" % (format(rAvg, '02x'), format(gAvg, '02x'), format(bAvg, '02x'))
+    else:
+        imagecolor = "FFF0F0F0"
+    log("Average Color: " + imagecolor)
+    return imagecolor
+
+from PIL import ImageFilter
+
+
+class MyGaussianBlur(ImageFilter.Filter):
+    name = "GaussianBlur"
+
+    def __init__(self, radius=2):
+        self.radius = radius
+
+    def filter(self, image):
+        return image.gaussian_blur(self.radius)
+
