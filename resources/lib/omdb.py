4f644eedc43ad55a3a8301f6fb1d19c8ad3e44fd
Philipp Te <philipptemminghoff@gmx.de>
2015-01-09 01:47:21 +0100

    omdb: fix empty labels

--- resources/lib/omdb.py
+++ resources/lib/omdb.py
@@ -6,6 +6,9 @@ def GetOmdbMovieInfo(imdb_id):
     try:
         url = 'i=%s' % (imdb_id)
         results = Get_JSON_response(base_url + url)
+        for (key, value) in results.iteritems():
+            if value == "N/A":
+                results[key] = ""
     except:
         results = None
         Notify("Error when fetching Omdb data from net")
