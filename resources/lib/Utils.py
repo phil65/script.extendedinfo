952df9ce2e76d0b1f589994c7fb88fb0a91f735b
Philipp Te <philipptemminghoff@gmx.de>
2015-01-18 02:08:52 +0100

    updated http header

--- resources/lib/Utils.py
+++ resources/lib/Utils.py
@@ -630,7 +630,7 @@ def GetStringFromUrl(url):
     while (succeed < 5) and (not xbmc.abortRequested):
         try:
             request = urllib2.Request(url)
-            request.add_header('User-agent', 'XBMC/13.2 ( ptemming@gmx.net )')
+            request.add_header('User-agent', 'XBMC/14.0 ( phil65@kodi.tv )')
             response = urllib2.urlopen(request)
             data = response.read()
             return data
