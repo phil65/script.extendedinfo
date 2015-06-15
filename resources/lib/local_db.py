4b6cecbae8093700103294bb611e38413c123a23
phil65 <philipptemminghoff@gmx.de>
2015-06-15 07:58:13 +0200

    more work for local tvshows

--- resources/lib/local_db.py
+++ resources/lib/local_db.py
@@ -107,10 +107,14 @@ def get_db_movies(filter_string="", limit=10):
     props = '"properties": ["title", "originaltitle", "votes", "playcount", "year", "genre", "studio", "country", "tagline", "plot", "runtime", "file", "plotoutline", "lastplayed", "trailer", "rating", "resume", "art", "streamdetails", "mpaa", "director", "writer", "cast", "dateadded"]'
     json_response = get_kodi_json('"method": "VideoLibrary.GetMovies", "params": {%s, %s, "limits": {"end": %d}}' % (props, filter_string, limit))
     if "result" in json_response and "movies" in json_response["result"]:
-        movies = []
-        for item in json_response["result"]["movies"]:
-            movies.append(handle_db_movies(item))
-        return movies
+        return [handle_db_movies(item) for item in json_response["result"]["movies"]]
+
+
+def get_db_tvshows(filter_string="", limit=10):
+    props = '"properties": ["title", "genre", "year", "rating", "plot", "studio", "mpaa", "cast", "playcount", "episode", "imdbnumber", "premiered", "votes", "lastplayed", "fanart", "thumbnail", "file", "originaltitle", "sorttitle", "episodeguide", "season", "watchedepisodes", "dateadded", "tag", "art"]'
+    json_response = get_kodi_json('"method": "VideoLibrary.GetTVShows", "params": {%s, %s, "limits": {"end": %d}}' % (props, filter_string, limit))
+    if "result" in json_response and "tvshows" in json_response["result"]:
+        return [handle_db_tvshows(item) for item in json_response["result"]["tvshows"]]
 
 
 def handle_db_movies(movie):
@@ -200,6 +204,11 @@ def get_movie_from_db(movie_id):
         return handle_db_movies(response["result"]["moviedetails"])
     return {}
 
+def get_tvshow_from_db(tvshow_id):
+    response = get_kodi_json('"method": "VideoLibrary.GetTVShowDetails", "params": {"properties": ["title", "genre", "year", "rating", "plot", "studio", "mpaa", "cast", "playcount", "episode", "imdbnumber", "premiered", "votes", "lastplayed", "fanart", "thumbnail", "file", "originaltitle", "sorttitle", "episodeguide", "season", "watchedepisodes", "dateadded", "tag", "art"], "tvshowid":%s }' % str(tvshow_id))
+    if "result" in response and "tvshowdetails" in response["result"]:
+        return handle_db_movies(response["result"]["tvshowdetails"])
+    return {}
 
 def get_kodi_albums():
     json_response = get_kodi_json('"method": "AudioLibrary.GetAlbums", "params": {"properties": ["title"]}')
