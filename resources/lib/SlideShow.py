fd91ccfab88dc9c9ecd67ef2de1249ce85c56303
phil65 <philipptemminghoff@gmx.de>
2015-06-24 01:25:08 +0200

    add new class files

---
+++ resources/lib/SlideShow.py
@@ -0,0 +1,34 @@
+import xbmcgui
+from Utils import *
+
+
+class SlideShow(xbmcgui.WindowXMLDialog):
+    ACTION_PREVIOUS_MENU = [9, 92, 10]
+    ACTION_LEFT = [1]
+    ACTION_RIGHT = [2]
+
+    def __init__(self, *args, **kwargs):
+        self.imagelist = kwargs.get('imagelist')
+        self.index = kwargs.get('index')
+        self.image = kwargs.get('image')
+        self.action = None
+
+    def onInit(self):
+        if self.imagelist:
+            self.getControl(10000).addItems(create_listitems(self.imagelist))
+            xbmc.executebuiltin("Control.SetFocus(10000,%s)" % self.index)
+        else:
+            listitem = {"label": self.image,
+                        "thumb": self.image}
+            self.getControl(10000).addItems(create_listitems([listitem]))
+
+    def onAction(self, action):
+        if action in self.ACTION_PREVIOUS_MENU:
+            self.close()
+        elif action in self.ACTION_LEFT:
+            self.action = "left"
+            self.close()
+        elif action in self.ACTION_RIGHT:
+            self.action = "right"
+            self.close()
+
