a9a2fbd218320475072ca3d324a292251dfe442a
phil65 <philipptemminghoff@gmx.de>
2015-11-24 12:49:09 +0100

    youtube: additional check

--- resources/lib/YouTube.py
+++ resources/lib/YouTube.py
@@ -49,9 +49,10 @@ def handle_youtube_videos(results, extended=False):
             item["viewcount"] = millify(ext_item['statistics']['viewCount'])
             item["likes"] = ext_item['statistics'].get('likeCount')
             item["dislikes"] = ext_item['statistics'].get('dislikeCount')
-            vote_count = float(int(item["likes"]) + int(item["dislikes"]))
-            if vote_count > 0:
-                item["rating"] = format(float(item["likes"]) / vote_count * 10, '.2f')
+            if item["likes"] and item["dislikes"]:
+                vote_count = float(int(item["likes"]) + int(item["dislikes"]))
+                if vote_count > 0:
+                    item["rating"] = format(float(item["likes"]) / vote_count * 10, '.2f')
             break
         else:
             item["duration"] = ""
