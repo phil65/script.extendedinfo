488915188231e6e134b942d9e58e58957ac67b23
phil65 <philipptemminghoff@gmx.de>
2015-12-21 06:06:24 +0100

    also search by name when using info=openinfodialog

--- resources/lib/process.py
+++ resources/lib/process.py
@@ -338,9 +338,9 @@ def start_info_actions(infos, params):
             if not dbid:
                 dbid = xbmc.getInfoLabel("ListItem.Property(dbid)")
             if xbmc.getCondVisibility("Container.Content(movies)"):
-                xbmc.executebuiltin("RunScript(script.extendedinfo,info=extendedinfo,dbid=%s,id=%s)" % (dbid, xbmc.getInfoLabel("ListItem.Property(id)")))
+                xbmc.executebuiltin("RunScript(script.extendedinfo,info=extendedinfo,dbid=%s,id=%s,name=%s)" % (dbid, xbmc.getInfoLabel("ListItem.Property(id)"), xbmc.getInfoLabel("ListItem.Title")))
             elif xbmc.getCondVisibility("Container.Content(tvshows)"):
-                xbmc.executebuiltin("RunScript(script.extendedinfo,info=extendedtvinfo,dbid=%s,id=%s)" % (dbid, xbmc.getInfoLabel("ListItem.Property(id)")))
+                xbmc.executebuiltin("RunScript(script.extendedinfo,info=extendedtvinfo,dbid=%s,id=%s,name=%s)" % (dbid, xbmc.getInfoLabel("ListItem.Property(id)"), xbmc.getInfoLabel("ListItem.Title")))
             elif xbmc.getCondVisibility("Container.Content(seasons)"):
                 xbmc.executebuiltin("RunScript(script.extendedinfo,info=seasoninfo,tvshow=%s,season=%s)" % (xbmc.getInfoLabel("ListItem.TVShowTitle"), xbmc.getInfoLabel("ListItem.Season")))
             elif xbmc.getCondVisibility("Container.Content(actors) | Container.Content(directors)"):
