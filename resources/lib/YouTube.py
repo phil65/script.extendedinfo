a8e0a8bc2ccb88a9661f5af32af4e5419db19318
phil65 <philipptemminghoff@gmx.de>
2016-02-17 16:34:57 +0100

    use itertools.product for nested loops

--- resources/lib/YouTube.py
+++ resources/lib/YouTube.py
@@ -84,10 +84,9 @@ def handle_playlists(results):
               "part": "contentDetails"}
     ext_results = get_data(method="playlists",
                            params=params)
-    for item in playlists:
-        for ext_item in ext_results["items"]:
-            if item["youtube_id"] == ext_item['id']:
-                item["itemcount"] = ext_item['contentDetails']['itemCount']
+    for item, ext_item in itertools.product(playlists, ext_results["items"]):
+        if item["youtube_id"] == ext_item['id']:
+            item["itemcount"] = ext_item['contentDetails']['itemCount']
     return playlists
 
 
@@ -114,11 +113,10 @@ def handle_channels(results):
               "part": "contentDetails,statistics,brandingSettings"}
     ext_results = get_data(method="channels",
                            params=params)
-    for item in channels:
-        for ext_item in ext_results["items"]:
-            if item["youtube_id"] == ext_item['id']:
-                item["itemcount"] = ext_item['statistics']['videoCount']
-                item["fanart"] = ext_item["brandingSettings"]["image"].get("bannerTvMediumImageUrl", "")
+    for item, ext_item in itertools.product(channels, ext_results["items"]):
+        if item["youtube_id"] == ext_item['id']:
+            item["itemcount"] = ext_item['statistics']['videoCount']
+            item["fanart"] = ext_item["brandingSettings"]["image"].get("bannerTvMediumImageUrl", "")
     return channels
 
 
