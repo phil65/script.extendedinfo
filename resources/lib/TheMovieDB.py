f5b98b3e94bb7e3bf783e5c0d914f1cefe55a8ad
phil65 <philipptemminghoff@gmx.de>
2016-03-13 16:16:18 +0100

    same fix for tvshows

--- resources/lib/TheMovieDB.py
+++ resources/lib/TheMovieDB.py
@@ -363,6 +363,7 @@ def handle_tvshows(results, local_first=True, sortkey="year"):
     for tv in results:
         tmdb_id = fetch(tv, 'id')
         genres = [labels[ids.index(id_)] for id_ in tv.get("genre_ids", []) if id_ in ids]
+        genres = [i for i in genres if i]
         duration = ""
         if "episode_run_time" in tv:
             if len(tv["episode_run_time"]) > 1:
