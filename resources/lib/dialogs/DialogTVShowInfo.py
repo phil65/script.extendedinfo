fa731d67f4dd4b1767d027966818371ac1a2bbf1
phil65 <philipptemminghoff@gmx.de>
2016-04-05 23:37:04 +0200

    another fix

--- resources/lib/dialogs/DialogTVShowInfo.py
+++ resources/lib/dialogs/DialogTVShowInfo.py
@@ -51,7 +51,7 @@ def get_window(window_type):
                 return None
             self.info, self.data, self.account_states = data
             if "dbid" not in self.info:
-                self.info['poster'] = Utils.get_file(self.info.get("poster", ""))
+                self.info.set_art("poster", Utils.get_file(self.info.get("poster", "")))
             self.info["properties"].update(ImageTools.blur(self.info["artwork"].get("poster")))
             self.listitems = [(ID_LIST_SIMILAR, self.data["similar"]),
                               (ID_LIST_SEASONS, self.data["seasons"]),
