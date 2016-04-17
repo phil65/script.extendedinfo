18d5d8beff49b489cf2a5783ee29d1804436cdbe
phil65 <philipptemminghoff@gmx.de>
2016-04-17 21:31:01 +0200

    cleanup

--- resources/lib/omdb.py
+++ resources/lib/omdb.py
@@ -9,10 +9,8 @@ BASE_URL = "http://www.omdbapi.com/?tomatoes=true&plot=full&r=json&"
 
 
 def get_movie_info(imdb_id):
-    try:
-        url = 'i=%s' % (imdb_id)
-        results = utils.get_JSON_response(BASE_URL + url, 20, "OMDB")
-        return dict((k, v) for (k, v) in results.iteritems() if v != "N/A")
-    except Exception:
-        utils.log("Exception: Error when fetching Omdb data from net")
-        return {}
+    url = 'i=%s' % (imdb_id)
+    results = utils.get_JSON_response(BASE_URL + url, 20, "OMDB")
+    if not results:
+        return None
+    return {k: v for (k, v) in results.iteritems() if v != "N/A"}
